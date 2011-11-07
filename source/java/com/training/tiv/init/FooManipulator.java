f0abf8dab72f82394cc57abcd63799b97bf0eacc
Vadim Kulinski <kulinskyvs@gmail.com>
2011-11-07 12:43:30 +0300

    Adding initialization examples (including inheritance mechanism)

--- source/java/com/training/tiv/init/FooManipulator.java
+++ source/java/com/training/tiv/init/FooManipulator.java
@@ -3,26 +3,26 @@ package com.training.tiv.init;
 public class FooManipulator {
 
  static {
-   fooSt2 = new Foo("st2");
+   fooSt2 = new Foo("ST_2");
  }
 
- //static variables - initialization is performed only a sinlge time
- private static Foo fooSt1 = new Foo("st1");
+ //static variables - initialization is performed only once
+ private static Foo fooSt1 = new Foo("ST_1");
  private static Foo fooSt2;
 
  //instance variables - initialization is performed any time a class instance is created
- private Foo foo1 = new Foo("obj1"); 
+ private Foo foo1 = new Foo("obj_1"); 
  private Foo foo2;
  private Foo foo3;
 
  {
-   foo3 = new Foo("obj3");
+   foo3 = new Foo("obj_3");
  } 
 
  
  public FooManipulator() {
-   System.out.println("from constructor----");
-   foo2 = new Foo("obj2");
+   System.out.println("----from constructor----");
+   foo2 = new Foo("obj_2");
    System.out.println("object construction finished!");
  }
 
@@ -34,17 +34,36 @@ public class FooManipulator {
    System.out.println("-----------------------------");
 
    //note that fooStM1 has package access > it's possible to use this variable here
-   Foo f1 = FooManipulator1.fooStM1;
+   //Foo f1 = FooManipulator1.fooStM1;
+
+   //check initialization order using inheritance engine
+   System.out.println("\n\n-----------------------------");
+   System.out.println("-- check inheritance ---");
+   new FooManipulator1();
   }
 
 }
 
 /**
-  * The class is placed in one source module (FooManipulator.java) - 
+  * The class is placed in the same source module (FooManipulator.java) - 
   * but the class in compiled in a separate .class file in the package "com.training.tiv.init".
   */
-class FooManipulator1 {
+class FooManipulator1 extends FooManipulator{
   
-  static Foo fooStM1 = new Foo("man1St1");
-
+  static Foo fooStM1 = new Foo("MAN_ST_1");
+  
+  {
+     obj5 = new Foo("MAN_OBJ_5");
+  }
+  
+  private Foo obj4 = new Foo("MAN_OBJ_4");
+  private Foo obj5;
+  private Foo obj6;
+  
+  public FooManipulator1() {
+     //note that default constuctor of base classes is executed automatically
+     System.out.println("----from descendant constructor  ----");
+     obj6 = new Foo("MAN_OBJ_6");
+     System.out.println("descendant object construction finished!");
+  }
 }
